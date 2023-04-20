d778321a23b9deb58884840f1d1af29aefa91741
Yash <yashlotan7@gmail.com>
2023-04-20 16:45:37 +0530

    Added image listing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,51 @@
-import { Flex, Heading, Text } from '@chakra-ui/react'
+import { AspectRatio, Flex, Grid, HStack, Image } from '@chakra-ui/react'
 
 const Home = () => {
+    const images = [
+        {
+            link: 'https://placehold.co/600x400/png',
+            title: 'abc',
+            starCount: 0,
+        },
+        {
+            link: 'https://placehold.co/500x400/png',
+            title: 'def',
+            starCount: 0,
+        },
+        {
+            link: 'https://placehold.co/300x400/png',
+            title: 'def',
+            starCount: 0,
+        },
+        {
+            link: 'https://placehold.co/700x400/png',
+            title: 'def',
+            starCount: 0,
+        },
+        {
+            link: 'https://placehold.co/200x400/png',
+            title: 'def',
+            starCount: 0,
+        },
+        {
+            link: 'https://placehold.co/300x400/png',
+            title: 'def',
+            starCount: 0,
+        },
+        {
+            link: 'https://placehold.co/600x400/png',
+            title: 'def',
+            starCount: 0,
+        },
+    ]
+
     return (
-        <Flex flexDirection={'column'}>
-            <Heading as="h1" size="4xl">
-                Imager
-            </Heading>
-            <Text>A simple sapp to share photos</Text>
+        <Flex flexDirection={'column'} py={4}>
+            <Flex flexWrap={'wrap'} id="images-wrapper">
+                {images.map((image, i) => {
+                    return <Image key={i} src={image.link} mr={4} mb={4} />
+                })}
+            </Flex>
         </Flex>
     )
 }
